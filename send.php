d64359c2878e1a6d78bcd7f0c641e6d8d6bea9ec
halysongoncalves <halyson.goncalves@concretesolutions.com.br>
2016-08-10 09:06:58 -0300

    Add server email

--- send.php
+++ send.php
@@ -1,4 +1,4 @@
-<?php
+	<?php
 if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']) && isset($_POST['message'])) {
 	//require("class.phpmailer.php"); //you have to download this plugin from github.com - link is in documentation
 	
@@ -6,20 +6,21 @@ if(isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['emai
 	$lastname = 	$_POST['lastname'];
 	$email = 		$_POST['email'];
 	$message = 		$_POST['message'];
-	
-	$mail = new PHPMailer();
-	
-	$mail->From = $email;
-	$mail->CharSet = "UTF-8";
-	$mail->FromName = $email;
-	$mail->addAddress("halysongoncalves@gmail.com"); 
-	
-	$mail->IsHTML(true);
-	$mail->Subject = $name." Contact me Web Site";
-	
-	$mail->Body = $message."<br><br>".$name."<br>".$email."<br>".$phone."<br>";
 
-	$mail->send();
+	require 'vendor/autoload.php';
+	use Mailgun\Mailgun;
+
+	# Instantiate the client.
+	$mgClient = new Mailgun('key-b07b77f2eded53e9e0388422b9b7dc1e');
+	$domain = "sandboxb8c9597fe99d4d3ba2a82e0f6cfd1cbd.mailgun.org";
+
+	# Make the call to the client.
+	$result = $mgClient->sendMessage("$domain",
+	                  array('from'    => 'Mailgun Sandbox <postmaster@sandboxb8c9597fe99d4d3ba2a82e0f6cfd1cbd.mailgun.org>',
+	                        'to'      => 'Halyson Lima <halysongoncalves@gmail.com>',
+	                        'subject' => 'Hello Halyson Lima',
+	                        'text'    => 'Congratulations Halyson Lima, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.'));
+	
 }
 ?>   
             
